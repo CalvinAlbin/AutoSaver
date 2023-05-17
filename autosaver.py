a9c138e98bf373f491f9522954a7b170cb03737d
CalvinAlbin <calvin.l.albin@gmail.com>
2023-05-16 19:44:17 -0500

    working autosave, cant execute stop or restart

--- autosaver.py
+++ autosaver.py
@@ -1,10 +1,46 @@
 from pynput.keyboard import Key, Controller
 import time
+import tkinter as tk
 
-
+trueCont = True
 keyboard = Controller()
+counterFix = 0
 
-while True:
-    keyboard.press(Key.f5)
-    keyboard.release(Key.f5)
-    time.sleep(5)
+
+def startAuto():
+    global counterFix
+    print(counterFix)
+    if counterFix == 0:
+        counterFix += 1
+        return
+    while trueCont == True:
+        keyboard.press(Key.f5)
+        keyboard.release(Key.f5)
+        time.sleep(5)
+
+def restartAuto():
+    print("RESTART")
+    trueCont = True
+
+def stopAuto():
+    print("STOP")
+    trueCont = False
+
+
+def main():
+    #global counterFix
+    global window
+    window = tk.Tk()
+    window.geometry('250x150')
+    startButton = tk.Button(window, text = "START AUTOSAVER", command = startAuto)
+    startButton.pack()
+
+    stopButton = tk.Button(window, text = "RESTART AUTOSAVER", command = stopAuto)
+    stopButton.pack()
+
+    restartButton = tk.Button(window, text = "STOP AUTOSAVER", command = restartAuto)
+    restartButton.pack()
+
+    window.mainloop()
+
+main()
